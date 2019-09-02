b5f333474782e65e72e4607b7df964a33cbc1352
Keseru Szabolcs <keserusz94@gmail.com>
2019-09-02 19:07:15 +0300

    studied inheritance

--- src/main/java/org/fasttrackit/Car.java
+++ src/main/java/org/fasttrackit/Car.java
@@ -1,16 +1,9 @@
 package org.fasttrackit;
+//inheritance (is-a relationship)
+public class Car extends AutoVehicle {
 
-public class Car {
 
-    String name;
-    double mileage;
-    double maxSpeed;
-    String fuelType;
-    double fuelLevel;
-    double traveledDistance;
-    int doorCount;
-    boolean running;
     //has-a relationship
-    Engine engine;
+    int doorCount;
 
 }
