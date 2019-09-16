655230c68bb53aab331426319ae4ced3b9346b18
Keseru Szabolcs <keserusz94@gmail.com>
2019-09-16 22:25:54 +0300

    studied list

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -1,15 +1,40 @@
 package org.fasttrackit;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Game {
 
     private Vehicle comptetitor1;
     private Vehicle compteritor2;
     private Track[] tracks = new Track[3];
+    private List<Vehicle> competitors = new ArrayList<>();
+
 
     public void start(){
         initializeTrack();
         displayTracks();
+        initializeCompetitors();
+    }
 
+    private void initializeCompetitors(){
+        int competitorCount = 2;
+
+        System.out.println("Today's competitors are: ");
+
+        for (int i = 0; i < competitorCount; i++){
+            Vehicle competitor = new Vehicle();
+            competitor.setName("Competitor " + i);
+            competitor.setMaxSpeed(300);
+            competitor.setMileage(ThreadLocalRandom.current().nextDouble(6,12));
+            competitor.setFuelLevel(80);
+
+            System.out.println(competitor);
+            competitors.add(competitor);
+
+        }
     }
 
     private void initializeTrack(){
